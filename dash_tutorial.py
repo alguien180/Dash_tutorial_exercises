34bf3ef68a87e07a864814227a4753f0563aeb19
Miguel Garza <miguel_agj@protonmail.com>
2026-01-29 22:11:18 +0100

    Requirements

--- dash_tutorial.py
+++ dash_tutorial.py
@@ -1,9 +1,28 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import pandas as pd
+import dash_ag_grid as dag
+import plotly.express as px
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 app = Dash()
 
-app.layout=html.Div(children="testing the app")
+app.layout= [
+    html.Div(children="testing the app"),
+    dag.AgGrid(
+        rowData=df.to_dict('records'),
+        columnDefs=[{"field":i} for i in df.columns]
+    ),
+    dcc.Graph(figure=px.histogram(df,x='continent',y='lifeExp',histfunc='avg'))
+]
+
+
+
+
+
+
 
 if __name__ =='__main__':
     app.run(debug=True)
+
