c2bc391442f6bc10cb16b044ae7b3d27838a8831
Miguel Garza <miguel_agj@protonmail.com>
2026-01-29 22:41:09 +0100

    Added basic visualizations

--- dash_tutorial.py
+++ dash_tutorial.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, callback, Output, Input, dcc
 import pandas as pd
 import dash_ag_grid as dag
 import plotly.express as px
@@ -16,8 +16,14 @@ app.layout= [
     ),
     dcc.Graph(figure=px.histogram(df,x='continent',y='lifeExp',histfunc='avg'))
 ]
+@callback(
+    Output(component_id='controls-and-graph',component_property='figure'),
+    Input(component_id='controls-and-radio-item',component_propery='value')
 
-
+)
+def update_graph(col_chosen):
+    fig= px.histogram(df,x='continent',y=col_chosen,histfunc='avg')
+    return fig
 
 
 
