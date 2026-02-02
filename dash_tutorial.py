6650d298fcec2a21f171dab28f84cbbbdb258780
Miguel Garza <miguel_agj@protonmail.com>
2026-02-02 21:44:03 +0100

    -

--- dash_tutorial.py
+++ dash_tutorial.py
@@ -1,34 +1,54 @@
-from dash import Dash, html, callback, Output, Input, dcc
-import pandas as pd
+# Import packages
+from dash import Dash, html, dcc, callback, Output, Input
 import dash_ag_grid as dag
+import pandas as pd
 import plotly.express as px
+import dash_bootstrap_components as dbc
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-app = Dash()
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout= [
-    html.Div(children="testing the app"),
-    dag.AgGrid(
-        rowData=df.to_dict('records'),
-        columnDefs=[{"field":i} for i in df.columns]
-    ),
-    dcc.Graph(figure=px.histogram(df,x='continent',y='lifeExp',histfunc='avg'))
-]
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+    ]),
+
+    dbc.Row([
+        dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
+                       value='lifeExp',
+                       inline=True,
+                       id='radio-buttons-final')
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dag.AgGrid(
+                rowData=df.to_dict('records'),
+                columnDefs=[{"field": i} for i in df.columns]
+            )
+        ], width=6),
+
+        dbc.Col([
+            dcc.Graph(figure={}, id='my-first-graph-final')
+        ], width=6),
+    ]),
+
+], fluid=True)
+
+# Add controls to build the interaction
 @callback(
-    Output(component_id='controls-and-graph',component_property='figure'),
-    Input(component_id='controls-and-radio-item',component_propery='value')
-
+    Output(component_id='my-first-graph-final', component_property='figure'),
+    Input(component_id='radio-buttons-final', component_property='value')
 )
 def update_graph(col_chosen):
-    fig= px.histogram(df,x='continent',y=col_chosen,histfunc='avg')
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
 
-
-
-
-
-if __name__ =='__main__':
+# Run the app
+if __name__ == '__main__':
     app.run(debug=True)
-
